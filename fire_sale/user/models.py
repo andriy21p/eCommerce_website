090a8134c3cf3e7e766f6f44a043372cdbf096b5
Andriy Fedorets <andriy21@ru.is>
2022-05-02 21:33:55 +0000

    item

--- fire_sale/user/models.py
+++ fire_sale/user/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     name = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.CharField(max_length=1000)
-    bio = models.CharField(max_length=10000)
+    image = models.CharField(max_length=1000, blank=True)
+    bio = models.CharField(max_length=10000, blank=True)
 
     def __str__(self):
         return self.name
