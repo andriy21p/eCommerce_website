a947079d6d4a3794b90cce8f14a608aec6430ec4
Kristinn <kritinnjg20@ru.is>
2022-05-02 20:58:15 +0000

    items->item

--- fire_sale/items/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from items.models import *
-
-# Register your models here.
-
-admin.site.register(Items)
-admin.site.register(ItemCondition)
-admin.site.register(ItemsSalesType)
-admin.site.register(ItemCategory)
