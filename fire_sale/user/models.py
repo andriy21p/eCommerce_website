bbd86daf2b6ccaebb1b5080fa6362ea269c7ceed
Pall Thorsteinsson <pallarnar81@gmail.com>
2022-05-02 21:39:14 +0000

    User Profile now accepts entries

--- fire_sale/user/models.py
+++ fire_sale/user/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
 class Profile(models.Model):
     name = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.CharField(max_length=1000)
-    bio = models.CharField(max_length=10000)
+    image = models.CharField(max_length=1000, blank=True)
+    bio = models.CharField(max_length=10000, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.name.first_name
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender,instance, **kwargs):
+    instance.profile.save()
+
