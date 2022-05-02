0964bb388a9ecc13a52ec9750cc00955471fdb40
Pall Thorsteinsson <pallarnar81@gmail.com>
2022-05-02 20:56:38 +0000

    User model extended

--- fire_sale/user/models.py
+++ fire_sale/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.CharField(max_length=1000)
+    bio = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return self.name
