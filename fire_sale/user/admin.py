6aff8ffa6f597f52d7dd3386e1c2e0ff16d8ddfe
Pall Thorsteinsson <pallarnar81@gmail.com>
2022-05-02 21:15:02 +0000

    Profile now available in Admin

--- fire_sale/user/admin.py
+++ fire_sale/user/admin.py
@@ -1,6 +1,6 @@
-# from django.contrib import admin
-# from user.models import *
+from django.contrib import admin
+from user.models import *
 
 # Register your models here.
 
-# admin.site.register(User)
+admin.site.register(Profile)
--- fire_sale/user/views.py
+++ fire_sale/user/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
 
-# from items.models import Items
-
-
 # Create your views here.
 def index(request):
     return render(request, 'user/index.html', {
