8b699f005f277ed05d781655595743905e78097d
Thorsteinsson <Pall.Thorsteinsson@Teledyne.com>
2022-05-02 09:16:14 +0000

    database addded to settings.py. Missing public IP addresses from users and packages postgresql_psycopg2 needs to be installed into venv

--- fire_sale/fire_sale/settings.py
+++ fire_sale/fire_sale/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'fire_sale.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'firesale-db',
+        'USER': 'firesale-db-user',
+        'PASSWORD': 'xoCJg8o5dofKL2Fj',
+        'HOST': '34.142.71.203',
+        'PORT': '5432'
     }
 }
 
