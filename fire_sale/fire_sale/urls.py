18a32019ea66a0b1701a61f45ae1129fc101ccfd
Andriy Fedorets <andriy21@ru.is>
2022-05-02 19:15:29 +0000

    item

--- fire_sale/fire_sale/urls.py
+++ fire_sale/fire_sale/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('items.urls')),
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('items/', include('items.urls')),
