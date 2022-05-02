e6a6e17fca3f90a79d4ac6925b7a663305bb342e
Pall Thorsteinsson <pallarnar81@gmail.com>
2022-05-02 22:00:00 +0000

    Profile view has been added.

--- fire_sale/user/models.py
+++ fire_sale/user/models.py
@@ -21,6 +21,6 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender,instance, **kwargs):
+def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
