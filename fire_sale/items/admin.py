7c843236499782da911f2dce3f4c918fd9ea9cf0
Kristinn <kritinnjg20@ru.is>
2022-05-02 19:49:52 +0000

    Item model komið v0

--- fire_sale/items/admin.py
+++ fire_sale/items/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from items.models import *
 
 # Register your models here.
+
+admin.site.register(Items)
+admin.site.register(ItemCondition)
+admin.site.register(ItemsSalesType)
--- fire_sale/items/models.py
+++ fire_sale/items/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class ItemsSalesType(models.Model):
+    name = models.CharField(max_length=200)
+    buyItNow = models.BooleanField(default=False)
+    auction = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ItemCondition(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Items(models.Model):
+    sale_type = models.ForeignKey(ItemsSalesType, on_delete=models.CASCADE)
+    price_minimum = models.FloatField(default=0)
+    price_fixed = models.FloatField(null=True, blank=True)
+    Condition = models.ForeignKey(ItemCondition, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    Description = models.CharField(max_length=10000)
+    show_in_catalog = models.BooleanField(default=False)
+    date_ends = models.DateTimeField(null=True, blank=True)
+    created = models.DateTimeField(default=timezone.now())
+    edited = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.name
