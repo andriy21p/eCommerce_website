085216f52d6672b9d54f4bdb0a95646227deb1a6
Pall Thorsteinsson <pallarnar81@gmail.com>
2022-05-03 22:13:36 +0000

    First iteration of the Message model added and migrated.

--- fire_sale/message/models.py
+++ fire_sale/message/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Message(models.Model):
+    message_body = models.CharField(max_length=10000)
+    sender = models.ForeignKey(User, related_name="msg_sender", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name="msg_receiver", on_delete=models.CASCADE)
+    msg_subject = models.CharField(max_length=200, blank=True)
+    msg_sent = models.DateTimeField(default=timezone.now)
+    msg_received = models.DateTimeField(default=None)
+    msg_replied = models.DateTimeField(default=None)
+
+
+class MessageHistory(models.Model):
+    msg_history = models.ForeignKey(Message, on_delete=models.CASCADE)
