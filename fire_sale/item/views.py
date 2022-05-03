e96486d38103dce2df7c5be40495b4bad225feae
Kristinn <kritinnjg20@ru.is>
2022-05-03 20:44:45 +0000

    Category order

--- fire_sale/item/views.py
+++ fire_sale/item/views.py
@@ -5,7 +5,9 @@ from item.models import Item, ItemCategory
 # Create your views here.
 def index(request):
     return render(request, 'item/index.html', {
-        'items': Item.objects.all(), 'images': Item.itemimage_set, 'categories': ItemCategory.objects.all(),
+        'items': Item.objects.all(),
+        'images': Item.itemimage_set,
+        'categories': ItemCategory.objects.all().order_by('order'),
     })
 
 
