a5b5f71cebeeae43a94e1fde47f3e1c9d58b6bb6
Kristinn <kritinnjg20@ru.is>
2022-05-04 15:46:57 +0000

    Basic item details

--- fire_sale/item/urls.py
+++ fire_sale/item/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='item-index'),
+    path('<int:item_key>', views.get_item_by_id),
 ]
