22e5cbff2922a244cbb6d586d0b9e38ce26de6da
Kristinn <kritinnjg20@ru.is>
2022-05-05 20:12:24 +0000

    Edit profile picture

--- fire_sale/user/urls.py
+++ fire_sale/user/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login', LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout', LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('profile', views.profile, name='profile'),
+    path('image', views.image, name='profile-image'),
 ]
