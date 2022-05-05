e0e19409c229de40ba3257f4ea3d45ef2958c8f5
Pall Thorsteinsson <pallarnar81@gmail.com>
2022-05-05 19:24:40 +0000

    Messages will only display user messages when accessing /message

--- fire_sale/message/views.py
+++ fire_sale/message/views.py
@@ -10,35 +10,37 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'message/index.html', {
-        # 'message': Message.objects.all(),
+    """ When a message is requested, when a user is logged, all messages related to him will be displayed."""
+    current_user = request.user
+    return render(request, "message/index.html", {
+        "message": Message.objects.filter(receiver=current_user.id).order_by("msg_sent"),
     })
 
 
 def create_new_msg(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = MsgReplyForm(data=request.POST)
         if form.is_valid():
             new_message = form.save(commit=False)
             new_message.user = request.user
             new_message.save()
-            return redirect('my-profile')
-    return render(request, 'message/msg_create.html', {
-        'form': MsgReplyForm()
+            return redirect("my-profile")
+    return render(request, "message/msg_create.html", {
+        "form": MsgReplyForm()
     })
 
 
 def get_msg_by_id(request, msg_key):
     Message.objects.filter(id=msg_key)
     messages = [{
-        'id': msg.id,
-        'msg_body': msg.message_body,
-        'msg_sender': msg.sender.username,
-        'msg_receiver': msg.receiver.username,
-        'msg_subject': msg.msg_subject,
-        'msg_sent': msg.msg_sent,
-        'msg_received': msg.msg_received,
-        'msg_replied': msg.msg_replied
+        "id": msg.id,
+        "msg_body": msg.message_body,
+        "msg_sender": msg.sender.username,
+        "msg_receiver": msg.receiver.username,
+        "msg_subject": msg.msg_subject,
+        "msg_sent": msg.msg_sent,
+        "msg_received": msg.msg_received,
+        "msg_replied": msg.msg_replied
 
     } for msg in Message.objects.filter(pk=msg_key)]
-    return JsonResponse({'messages': messages})
+    return JsonResponse({"messages": messages})
