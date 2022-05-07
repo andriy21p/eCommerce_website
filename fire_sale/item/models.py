0b9a136a24169a49420bfee26d9cf836c2ff83c9
Kristinn <kritinnjg20@ru.is>
2022-05-07 13:39:51 +0000

    Laga smá PEP8 villur

--- fire_sale/item/models.py
+++ fire_sale/item/models.py
@@ -108,8 +108,10 @@ class Offer(models.Model):
                 dict(zip(columns, row))
                 for row in cursor.fetchall()
             ]
+
         """ 
-            A short explination of what this SQL command does. We are trying to get information on my current bid status on an item 
+            A short explination of what this SQL command does. We are trying to get information 
+            on my current bid status on an item :
                 item ID
                 number of my bids on item
                 my highest bid
@@ -118,10 +120,10 @@ class Offer(models.Model):
         """
         with connection.cursor() as cursor:
             cursor.execute('select max(o.amount) as "myMax", i.id, i.name, count(*) as "bids"' +
-                # ', (select o2.offer_by_id from item_offer o2 where o2.item_id = o.item_id order by o2.amount desc fetch first 1 row only) as "high_bidder" ' +
-                'from item_offer o ' +
-                'join item_item i on i.id = o.item_id ' +
-                'where o.offer_by_id = ' + str(user.id) + 'and o.item_id = ' + str(self.item.id) + ' and o.amount >= i.price_minimum ' +
-                'group by i.id, o.item_id')
+                           'from item_offer o ' +
+                           'join item_item i on i.id = o.item_id ' +
+                           'where o.offer_by_id = ' + str(user.id) + 'and o.item_id = ' +
+                           str(self.item.id) + ' and o.amount >= i.price_minimum ' +
+                           'group by i.id, o.item_id')
             return dictfetchall(cursor)
         return None
--- fire_sale/user/models.py
+++ fire_sale/user/models.py
@@ -23,4 +23,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-
