c151cb1ab85f0be50c32261255291abe78bf9ddd
Pall Thorsteinsson <pallarnar81@gmail.com>
2022-05-11 18:55:07 +0000

    Checkout is created, preview pages displays preview

--- fire_sale/checkout/views.py
+++ fire_sale/checkout/views.py
@@ -19,8 +19,23 @@ def index(request, order_id):
 
 
 def get_checkout_by_id(request, checkout_id):
-    checkout = get_object_or_404(Checkout, pk=checkout_id)
-    return render(request, 'checkout/preview.html', {"checkout", checkout})
+    #checkout = get_object_or_404(Checkout, pk=checkout_id)
+    checkout = [{
+        #"id": co_entry.id,
+        #"offer_id": co_entry.offer_id,
+        "Street Address:": co_entry.street_address,
+        "House Number:": co_entry.house_number,
+        "City:": co_entry.city,
+        "Country:": co_entry.country,
+        "Postal Code:": co_entry.postal_code,
+        "Credit card holder:": co_entry.credit_card_holder,
+        "Credit card number:": co_entry.credit_card_number,
+        "Expiration month:": co_entry.expiration_month,
+        "Expiration year:": co_entry.expiration_year,
+        "CVV:": co_entry.cvv,
+        #"is_confirmed": co_entry.is_confirmed
+    } for co_entry in Checkout.objects.filter(pk=checkout_id)]
+    return render(request, 'checkout/preview.html', {"checkout": checkout})
 
 
 def register_checkout(request, order_id):
@@ -33,8 +48,8 @@ def register_checkout(request, order_id):
         formdata['offer'] = order.offer_by.pk
         form = CheckoutForm(data=formdata)
         if form.is_valid():
-            form.save()
-            return redirect('preview', checkout_id=form['id'])
+            new_checkout = form.save()
+            return redirect('preview', checkout_id=new_checkout.pk)
 
     orderinstance = {'name': order.offer_by.pk, 'item': order.item_id}
     return render(request, 'checkout/index.html', {
