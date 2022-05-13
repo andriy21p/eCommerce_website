8cbcfdd99a3ca3010741770c84783493bef4593b
Kristinn <kritinnjg20@ru.is>
2022-05-13 19:42:13 +0000

    Fix login page redirect

--- fire_sale/fire_sale/settings.py
+++ fire_sale/fire_sale/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-LOGIN_URL = '/user/login/'
+LOGIN_URL = '/user/login'
 LOGIN_REDIRECT_URL = '/item'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
